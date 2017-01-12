2532cd6284250a7f64204ea411f122b5bb53e180
Francis Ian Lao <Francis Ian Lao>
2017-01-11 18:21:25 -0800

    cleaned up functions for logic

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,19 +1,19 @@
 let friends = [{
-  name: "Sasha",
-  photo: "http://www.wwe.com/f/styles/og_image/public/rd-talent/Bio/Sasha_Banks_bio.png",
-  scores: ["4","3","5","2","1","2","4","2","3","2"]
+    name: "Sasha",
+    photo: "http://www.wwe.com/f/styles/og_image/public/rd-talent/Bio/Sasha_Banks_bio.png",
+    scores: ["4","3","5","2","1","2","4","2","3","2"]
 }, {
-  name: "Charlotte",
-  photo: "http://www.wwe.com/f/styles/og_image/public/rd-talent/Bio/Charlotte_bio.png",
-  scores: ["2","3","1","4","5","4","2","4","3","4"]
+    name: "Charlotte",
+    photo: "http://www.wwe.com/f/styles/og_image/public/rd-talent/Bio/Charlotte_bio.png",
+    scores: ["2","3","1","4","5","4","2","4","3","4"]
 }, {
-  name: "Bayley",
-  photo: "http://www.wwe.com/f/styles/wwe_large/public/all/2016/10/Bayley_bio--8d02c95a354b7eb05cdb0654e0dde57c.jpg",
-  scores: ["1","2","3","4","5","5","4","3","2","1"]
+    name: "Bayley",
+    photo: "http://www.wwe.com/f/styles/wwe_large/public/all/2016/10/Bayley_bio--8d02c95a354b7eb05cdb0654e0dde57c.jpg",
+    scores: ["1","2","3","4","5","5","4","3","2","1"]
 }, {
-  name: "Becky",
-  photo: "http://www.wwe.com/f/styles/wwe_large/public/rd-talent/Bio/Becky_Lynch_bio.png",
-  scores: ["3","4","5","4","5","1","2","2","4","4"]
+    name: "Becky",
+    photo: "http://www.wwe.com/f/styles/wwe_large/public/rd-talent/Bio/Becky_Lynch_bio.png",
+    scores: ["3","4","5","4","5","1","2","2","4","4"]
 }]
 
 module.exports = friends;
--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -3,59 +3,67 @@ var friends = require("../data/friends");
 
 // Display all friends - provides JSON
 module.exports=(app)=>{
-  app.get("/api/friends", function(request, response) {
-    response.json(friends);
-  });
+    app.get("/api/friends", function(request, response) {
+        response.json(friends);
+    });
 
 // Create new friend - takes in JSON input
-  app.post("/api/friends", function(request, response) {
-    var newFriend = request.body;
+    app.post("/api/friends", function(request, response) {
+        var newFriend = request.body;
     
-    console.log(newFriend);
+        console.log(newFriend);
 
-    var temp = [];
-    var totalScore = [];
+        var friendsScores = [];
+        var totalScore = [];
 
-    var newFriendScores = setNumbers(newFriend.scores);
-    console.log("Coverted N: " + newFriendScores);
+        var newFriendScores = setStringsToNumbers(newFriend.scores);
+        console.log("Coverted N: " + newFriendScores);
 
-    for(var i = 0; i < friends.length; i++) {
-      temp[i] = setNumbers(friends[i].scores);
-      console.log("Coverted " + i + ": " + temp[i])
-      totalScore[i] = (getScore(temp[i], newFriendScores));
-      console.log("Total " + i + ": " + totalScore[i]);
+        for(var i = 0; i < friends.length; i++) {
+            friendsScores[i] = setStringsToNumbers(friends[i].scores);
+            console.log("Coverted " + i + ": " + friendsScores[i])
+            totalScore[i] = (getTotal(friendsScores[i], newFriendScores));
+            console.log("Total " + i + ": " + totalScore[i]);
+        }
+
+        console.log("Best match: " + friends[getLowestTotalIndex(totalScore)].name);
+        response.json(friends[getLowestTotalIndex(totalScore)]);
+
+        friends.push(newFriend);
+    });
+
+    function setStringsToNumbers(strings){
+        var numbers = [];
+        
+        for(var i = 0; i < 10; i++)
+            numbers[i] = parseInt(strings[i]);
+        
+        return numbers;
     }
 
-    var min = totalScore[0];
-    var minIndex = 0;
-
-    for (var i = 1; i < totalScore.length; i++) {
-      if (totalScore[i] < min) {
-        minIndex = i;
-        min = totalScore[i];
-      }
-    }
-
-    console.log("Best match: " + friends[minIndex].name);
-    response.json(friends[minIndex]);
-    friends.push(newFriend);
-  });
-
-  function setNumbers(scores){
-    var numbers = [];
-    for(var i = 0; i < 10; i++)
-    numbers[i] = parseInt(scores[i]);
-    return numbers;
+    function getTotal(numArray1, numArray2){
+        var difference = [];
+        var total = 0;
+        
+        for(var i = 0; i < 10; i++) {
+            difference[i] = Math.abs(numArray1[i] - numArray2[i]);
+            total += difference[i];   
+        }
+    
+        return total;
   }
 
-  function getScore(a, b){
-    var difference = [];
-    var total = 0;
-    for(var i = 0; i < 10; i++) {
-      difference[i] = Math.abs(a[i] - b[i]);
-      total += difference[i];   
+    function getLowestTotalIndex(numArray) {
+        var min = numArray[0];
+        var minIndex = 0;
+
+        for (var i = 1; i < numArray.length; i++) {
+            if (numArray[i] < min) {
+                minIndex = i;
+                min = numArray[i];
+            }
+        }
+
+        return minIndex;
     }
-    
-    return total;
-  }
 }
